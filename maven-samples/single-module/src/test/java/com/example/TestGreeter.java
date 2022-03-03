d21dc4c51daf02f6d494a9343c1ae7b8c4c7f2e1
srinivasmaddirala <srinivasmaddirala93@gmail.com>
2022-03-03 22:17:43 +0530

    maa fellira bhai

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello, we are fine man   ";
+    String someone = "Hello, swathi weds srinu ";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
