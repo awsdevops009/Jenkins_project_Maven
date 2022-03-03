0e04e40eb9f7e48e16c5ccdb9294de699980d6ff
srinivasmaddirala <srinivasmaddirala93@gmail.com>
2022-03-03 22:59:44 +0530

    new file added

--- maven-samples/single-module/src/test/java/com/example/TestGreeter2.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter2.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello, maddirala invites everyone ";
+    String someone = "Hello, srinu+swathi ";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
