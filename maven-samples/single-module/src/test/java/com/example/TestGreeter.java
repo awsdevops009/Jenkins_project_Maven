9a6e4454103e96b98ce37c2ee19574778fda3dfe
Hariera <98234184+Hariera@users.noreply.github.com>
2022-08-23 12:30:18 +0530

    added file

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello - This is Hari, Hoe are you ";
+    String someone = "Hello - This is again modified file ";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
