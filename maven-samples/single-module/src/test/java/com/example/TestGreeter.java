8e3b0e8d38a7ac0675c2c647859c40dc368be98e
Hariera <98234184+Hariera@users.noreply.github.com>
2022-08-24 12:52:42 +0530

    today modified

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test 
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello - This is Hari";
+    String someone = "Hello - Today Wednesday;
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
