a8ebd13b13f52c1c36dc0c78c10c90f3acc10704
Hariera <98234184+Hariera@users.noreply.github.com>
2022-08-25 11:46:28 +0530

    Files

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test 
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello - This is Hari, github webhook ADDED";
+    String someone = "Hello - This is Hari, github webhook for PR";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
